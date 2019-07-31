1312ac8fc91af2e711d7c970a29b2644c950b2dc
d0riangrey <Q852456357159q>
2019-07-31 23:34:32 +0300

    31/07/2019

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,67 +1,20 @@
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.api.methods.send.SendMessage;
-import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
-import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
-
-import java.util.logging.Level;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class Bot extends TelegramLongPollingBot {
 
-    /**
-     * Метод для приема сообщений.
-     * @param update Содержит сообщение от пользователя.
-     */
-    @Override
+    //когда бот получает сообдение, выполняется этот метод
     public void onUpdateReceived(Update update) {
-        String message = update.getMessage().getText();
-        sendMsg(update.getMessage().getChatId().toString(), message);
+
     }
 
-    /**
-     * Метод для настройки сообщения и его отправки.
-     * @param chatId id чата
-     * @param s Строка, которую необходимот отправить в качестве сообщения.
-     */
-    public synchronized void sendMsg(String chatId, String s) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.enableMarkdown(true);
-        sendMessage.setChatId(chatId);
-        sendMessage.setText(s);
-        try {
-            sendMessage(sendMessage);
-        } catch (TelegramApiException e) {
-            log.log(Level.SEVERE, "Exception: ", e.toString());
-        }
-    }
-
-    /**
-     * Метод возвращает имя бота, указанное при регистрации.
-     * @return имя бота
-     */
-    @Override
+    //получение иени бота
     public String getBotUsername() {
-        return ”BotName”;
+        return null;
     }
 
-    /**
-     * Метод возвращает token бота для связи с сервером Telegram
-     * @return token для бота
-     */
-    @Override
+    //получение токена бота
     public String getBotToken() {
-        return ”BotToken”;
-    }
-
-    public static void main(String[] args) {
-        ApiContextInitializer.init();
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-        try {
-            telegramBotsApi.registerBot(Bot.getBot());
-        } catch (TelegramApiRequestException e) {
-            e.printStackTrace();
-        }
+        return null;
     }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+
+public class Main {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegram = new TelegramBotsApi();
+
+    }
+}
